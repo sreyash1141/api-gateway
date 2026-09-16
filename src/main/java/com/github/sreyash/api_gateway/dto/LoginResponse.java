e91a3178ab243c9d09aab0fd4e89d6a2ccc0e24e
Sreyash Das <dassreyash2015@gmail.com>
2026-09-16 06:38:30 +0530

    feat: add User entity, UserRepository, and DTOs

---
+++ src/main/java/com/github/sreyash/api_gateway/dto/LoginResponse.java
@@ -0,0 +1,12 @@
+package com.github.sreyash.api_gateway.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponse {
+    private String token;
+    private String username;
+    private String role;
+}
---
+++ src/main/java/com/github/sreyash/api_gateway/model/User.java
@@ -0,0 +1,37 @@
+package com.github.sreyash.api_gateway.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public enum Role {
+        USER, ADMIN
+    }
+}
